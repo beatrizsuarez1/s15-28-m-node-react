92281fc5088bdcef78644ef69405b57597573ca4
camiloduquee <camiloduquee@gmail.com>
2024-06-06 16:31:05 -0400

    add :login

--- frontend/src/hooks/useAuth.tsx
+++ frontend/src/hooks/useAuth.tsx
@@ -1,4 +1,4 @@
-// import { useNavigate } from "react-router-dom"
+import { useNavigate } from "react-router-dom"
 import { registerRequest, loginRequest } from "../api/auth";
 import { FormLogin, FRWithoutConfirm, useRequestType } from "../types";
 import { useAuth } from "../context/auth-context";
@@ -6,28 +6,31 @@ import { useAuth } from "../context/auth-context";
 
 
 const useRequest = (): useRequestType => {
-     
-    // const navigate = useNavigate();
-    const { isLoggedIn, login, logout} = useAuth()
+
+    const navigate = useNavigate();
+    const { isLoggedIn, login, logout } = useAuth()
 
     const loginUser = async (data: FormLogin): Promise<void> => {
+        console.log(data)
         try {
             const response = await loginRequest(data)
             console.log(response)
             login()
+            navigate('/dashboard')
         } catch (error: any) {
             console.log(error.message)
         }
     }
 
-    const registerUser = async (data:FRWithoutConfirm): Promise<void> => {
-        
+    const registerUser = async (data: FRWithoutConfirm): Promise<void> => {
+
         console.log(data)
         try {
-            
+
             const response = await registerRequest(data)
-            console.log(response)
-            // login()
+            console.log(response.data)
+            login()
+            navigate('/dashboard')
         } catch (error: any) {
             console.log(error.message)
         }
