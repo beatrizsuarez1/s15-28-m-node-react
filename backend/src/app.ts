1d0d0b4a8ee48487f91dbc72fd95736de8c36f09
FrancoEspinozaV <franco.espinoza.val@gmail.com>
2024-06-11 22:19:26 -0400

    move lines

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -16,12 +16,12 @@ app.use(cookieParser())
 app.disable('x-powered-by')
 
 // Routes
-const routesWithoutApiKey = ['/api/v1/docs', '/api/docs', '/']
-app.use(excludeRoutes(routesWithoutApiKey, isApiKey))
-app.use(router)
-
 app.get('/', (_, res) => {
   res.send('Hello World!')
 })
+
+const routesWithoutApiKey = ['/api/v1/docs', '/api/docs', '/']
+app.use(excludeRoutes(routesWithoutApiKey, isApiKey))
+app.use(router)
 
 export default app
